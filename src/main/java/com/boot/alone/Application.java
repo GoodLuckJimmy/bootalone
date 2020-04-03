6de5d2e1182e1020fcdf61e44a6b1f7a2857c974
seokhan <seok.han@bringprice.com>
2020-04-03 18:18:35 +0900

    구글, 네이버 로그인 연동

--- src/main/java/com/boot/alone/Application.java
+++ src/main/java/com/boot/alone/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing // test를 위해 JpaConfig로 분리
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/boot/alone/web/IndexController.java
+++ src/main/java/com/boot/alone/web/IndexController.java
@@ -1,22 +1,31 @@
 package com.boot.alone.web;
 
+import com.boot.alone.config.auth.LoginUser;
+import com.boot.alone.config.auth.dto.SessionUser;
 import com.boot.alone.service.PostsService;
 import com.boot.alone.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
